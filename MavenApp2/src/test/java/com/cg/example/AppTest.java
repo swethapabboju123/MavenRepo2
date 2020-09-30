80f6096e9c05d5d9d8d3f681c977c175c2a8aeab
spabboju <spabboju@LIN86000053.corp.capgemini.com>
2020-09-30 14:06:07 +0530

    Updated Changes

--- MavenApp2/src/test/java/com/cg/example/AppTest.java
+++ MavenApp2/src/test/java/com/cg/example/AppTest.java
@@ -1,31 +1,38 @@
 package com.cg.example;
 
-import org.junit.Test;
 
-//import junit.framework.Test;
+
+import static org.junit.Assert.fail;
+
+/*import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;*/
+import org.junit.Test; 
 
 /**
  * Unit test for simple App.
  */
-//import org.junit.Test; 
 public class AppTest 
 {
-	@Test
+	@org.junit.Test
 	public void test1() {
 		System.out.println("hello");
 	}
-	@Test
+	@org.junit.Test
 	public void test2() {
 		System.out.println("hi");
 	}
-	@Test
+	@org.junit.Test
 	public void test3() {
 		System.out.println("test3");
 			}
-	@Test
+	@org.junit.Test
 	public void test4() {
 		System.out.println("test5");
 		//fail();
 	}
 		
 }
+
+		
+
