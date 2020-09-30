78609c4745bbde342ef5805495970130e3aaaa1a
spabboju <spabboju@LIN86000053.corp.capgemini.com>
2020-09-30 13:12:37 +0530

    myrepo2

---
+++ MavenApp2/src/test/java/com/cg/example/AppTest.java
@@ -0,0 +1,31 @@
+package com.cg.example;
+
+import org.junit.Test;
+
+//import junit.framework.Test;
+
+/**
+ * Unit test for simple App.
+ */
+//import org.junit.Test; 
+public class AppTest 
+{
+	@Test
+	public void test1() {
+		System.out.println("hello");
+	}
+	@Test
+	public void test2() {
+		System.out.println("hi");
+	}
+	@Test
+	public void test3() {
+		System.out.println("test3");
+			}
+	@Test
+	public void test4() {
+		System.out.println("test5");
+		//fail();
+	}
+		
+}
